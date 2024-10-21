9a55e107d7ebfc297883677a21085c037dbbed92
Sofiia Beklemishcheva <beklemishchevasofiya@gmail.com>
2024-10-21 23:58:24 +0200

    Experiment wich lensesConstructions images paths

--- src/mock/lensesConstructions/lensesConstructions.js
+++ src/mock/lensesConstructions/lensesConstructions.js
@@ -1,35 +1,35 @@
 const sphericalPlusFour= [
     {
-        link1:"https://optimistic-optyk-demo-sofiiabeklemishchevas-projects.vercel.app/assets/+4,00 1,5 Sferyczna_page-0001.jpg",
-        link2:"https://optimistic-optyk-demo-sofiiabeklemishchevas-projects.vercel.app/assets/+4,00 1,6 Sferyczna_pages-to-jpg-0001.jpg",
-        link3:"https://optimistic-optyk-demo-sofiiabeklemishchevas-projects.vercel.app/assets/+4,00 1,67 Sferyczna_page-0001.jpg",
-        link4:"https://optimistic-optyk-demo-sofiiabeklemishchevas-projects.vercel.app/assets/+4,00 1,74 Sferyczna_page-0001.jpg"
+        link1:"assets/+4,00 1,5 Sferyczna_page-0001.jpg",
+        link2:"/assets/+4,00 1,6 Sferyczna_pages-to-jpg-0001.jpg",
+        link3:"/assets/+4,00 1,67 Sferyczna_page-0001.jpg",
+        link4:"/assets/+4,00 1,74 Sferyczna_page-0001.jpg"
     }
 ]
 
 const asphericalPlusFour= [
 {
-        link1:"https://optimistic-optyk-demo-sofiiabeklemishchevas-projects.vercel.app/assets/+4,00 1,5 Asferyczna_page-0001.jpg",
-        link2:"https://optimistic-optyk-demo-sofiiabeklemishchevas-projects.vercel.app/assets/+4,00 1,6 Asferyczna_pages-to-jpg-0001.jpg",
-        link3:"https://optimistic-optyk-demo-sofiiabeklemishchevas-projects.vercel.app/assets/+4,00 1,67 Asferyczna_page-0001.jpg",
-        link4:"https://optimistic-optyk-demo-sofiiabeklemishchevas-projects.vercel.app/assets/+4,00 1,74 Asferyczna_page-0001.jpg"
+        link1:"/assets/+4,00 1,5 Asferyczna_page-0001.jpg",
+        link2:"/assets/+4,00 1,6 Asferyczna_pages-to-jpg-0001.jpg",
+        link3:"/assets/+4,00 1,67 Asferyczna_page-0001.jpg",
+        link4:"/assets/+4,00 1,74 Asferyczna_page-0001.jpg"
     }
 ]
 const sphericalPlusSix= [
 {
-        link1:"https://optimistic-optyk-demo-sofiiabeklemishchevas-projects.vercel.app/assets/+6,00 1,5 Sferyczna_page-0001.jpg",
-        link2:"https://optimistic-optyk-demo-sofiiabeklemishchevas-projects.vercel.app/assets/+6,00 1,6 Sferyczna_page-0001.jpg",
-        link3:"https://optimistic-optyk-demo-sofiiabeklemishchevas-projects.vercel.app/assets/+6,00 1,67 Sferyczna_page-0001.jpg",
-        link4:"https://optimistic-optyk-demo-sofiiabeklemishchevas-projects.vercel.app/assets/+6,00 1,74 Sferyczna_page-0001.jpg"
+        link1:"/assets/+6,00 1,5 Sferyczna_page-0001.jpg",
+        link2:"/assets/+6,00 1,6 Sferyczna_page-0001.jpg",
+        link3:"/assets/+6,00 1,67 Sferyczna_page-0001.jpg",
+        link4:"/assets/+6,00 1,74 Sferyczna_page-0001.jpg"
     }    
     ]
     
 const asphericalPlusSix= [
 {
-        link1:"https://optimistic-optyk-demo-sofiiabeklemishchevas-projects.vercel.app/assets/+6,00 1,5 Asferyczna_page-0001.jpg",
-        link2:"https://optimistic-optyk-demo-sofiiabeklemishchevas-projects.vercel.app/assets/+6,00 1,6 Asferyczna_page-0001.jpg",
-        link3:"https://optimistic-optyk-demo-sofiiabeklemishchevas-projects.vercel.app/assets/+6,00 1,67 Asferyczna_page-0001.jpg",
-        link4:"https://optimistic-optyk-demo-sofiiabeklemishchevas-projects.vercel.app/assets/+6,00 1,74 Asferyczna_page-0001.jpg"
+        link1:"/assets/+6,00 1,5 Asferyczna_page-0001.jpg",
+        link2:"/assets/+6,00 1,6 Asferyczna_page-0001.jpg",
+        link3:"/assets/+6,00 1,67 Asferyczna_page-0001.jpg",
+        link4:"/assets/+6,00 1,74 Asferyczna_page-0001.jpg"
     }    
     ]
 
